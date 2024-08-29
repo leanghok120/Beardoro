578780e06a73208e202fd5da77ffc64be345dd15
leanghok120 <leanghokoeng5@gmail.com>
2024-08-30 04:59:29 +0700

    Add buttons (might change later)

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,5 +1,20 @@
 function Timer() {
-  return <h1 className="font-bold text-white text-9xl">50:00</h1>;
+  return (
+    <div>
+      <div className="flex items-center gap-4">
+        <button className="text-black font-bold bg-white px-4 py-2 rounded-full text-lg pointer hover:bg-white">
+          pomodoro
+        </button>
+        <button className="text-black font-bold bg-white px-4 py-2 rounded-full text-lg pointer hover:bg-white">
+          short break
+        </button>
+        <button className="text-black font-bold bg-white px-4 py-2 rounded-full text-lg pointer hover:bg-white">
+          long break
+        </button>
+      </div>
+      <h1 className="font-bold text-white text-center text-9xl mt-8">50:00</h1>
+    </div>
+  );
 }
 
 export default Timer;
